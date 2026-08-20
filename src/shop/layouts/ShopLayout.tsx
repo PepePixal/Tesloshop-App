acdc5877c31f52b08beefd1be2b9cc03ad6670ec
Pepe-Asus <pepepixal@gmail.com>
2026-08-20 11:50:30 +0200

    diseño-basico-homepage

--- src/shop/layouts/ShopLayout.tsx
+++ src/shop/layouts/ShopLayout.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router"
+import { CustomHeader } from "../components/CustomHeader"
+import { CustomFooter } from "../components/CustomFooter"
 
 
 export const ShopLayout = () => {
   return (
-    <div>
-      ShopLayout
+    <div className="min-h-screen bg-background">
 
+      <CustomHeader />
+     
       {/* Renderiza la ruta hija coincidente de una ruta padre o nada si ninguna ruta hija coincide. */}
       <Outlet />
+
+      <CustomFooter />
 
     </div>
 
--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,15 +1,12 @@
-import { Button } from "@/components/ui/button"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
 
 
 export const HomePage = () => {
   return (
-    <div>
-      <h1 className="font-montserrat">HomePage</h1>
-
-      <Button>
-        Botón
-      </Button>
-
-    </div>
+    <>
+    
+    <CustomJumbotron title='Todos los productos' />
+    
+    </>
   )
 }
